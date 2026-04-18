4671a04d35ef91e7af877f5c2ec91c41e69c2753
Farith-786 <farithahamed736@gmail.com>
2026-04-18 15:09:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ if name:
 number= st.slider("Pick a age",0,100)
 st.write(f"Your age is {number}")
 if st.button("Click me!"):
-    st.write("Button clicked!")
+    st.write("Most Welcome!")
 if st.checkbox("Show more info"):
     st.write("Assalamu alaikum Guys very Most Welcome to my 1st App development.")
