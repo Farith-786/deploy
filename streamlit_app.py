8740445a9ad01dc1fadc552b75cfc1cf86f62b69
Farith-786 <farithahamed736@gmail.com>
2026-04-18 14:54:55 +0530

    Create a basic Streamlit app with user input

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("This is my first Streamlit app")
+st.header("Welcome to Streamlit!")
+st.write("This is a basic Streamlit app.")
+
+name=st.text_input("Enter your name:")
+if name:
+    st.success(f"Hello, {name}! \nHow are you?")
+    
+number= st.slider("Pick a age",0,100)
+st.write(f"Your age is {number}")
+if st.button("Click me!"):
+    st.write("Button clicked!")
