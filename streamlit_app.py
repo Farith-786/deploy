b90aff7fefd17537a86b6dc612ff36cb8f1c8a16
Farith-786 <farithahamed736@gmail.com>
2026-04-18 17:27:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,7 @@ if st.button("Celebrate 🎉"):
         st.markdown(f"<h2 style='color: gold;'>Happy Birthday {name}! 🎊</h2>", unsafe_allow_html=True)
         st.balloons()
         st.snow()
+        if st.button("Click me!"):
+    st.write("Button clicked!")
+if st.checkbox("Show more info"):
+    st.write("Assalamu alaikum Guys very Most Welcome to my 1st App development.")
