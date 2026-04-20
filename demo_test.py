72fa8926caf767ef51300323589426f8fe01e090
Farith-786 <farithahamed736@gmail.com>
2026-04-20 13:15:38 +0530

    Update demo_test.py

--- demo_test.py
+++ demo_test.py
@@ -3,3 +3,14 @@ import pandas as pd
 
 st.title("My First develop App")
 demo = st.text_input("Enter your name")
+dataset = st.file_uploader("Upload your dataset", type=["csv"])
+if dataset is not None:
+    df = pd.read_csv(dataset)
+    st.dataframe(df)
+
+    if st.checkbox("Show Summary"):
+        st.write(df.describe())
+
+    if st.checkbox("Show Plot"):
+        fig = px.scatter(df, x=df.columns[0], y=df.columns[1])
+        st.plotly_chart(fig)
