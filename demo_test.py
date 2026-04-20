387c3bed32eebeb507890d0139a3885d074e5a70
Farith-786 <farithahamed736@gmail.com>
2026-04-20 17:26:23 +0530

    Update demo_test.py

--- demo_test.py
+++ demo_test.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
-import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
